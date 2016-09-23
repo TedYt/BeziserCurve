553126f3c22654e67b2d55b78fd47d3fe12e6266
tao.yu <zale.yu@tinno.com>
2016-09-23 19:31:34 +0800

    < Bezier Curve and Animation >

--- app/src/main/java/com/ted/beziercurve/MainActivity.java
+++ app/src/main/java/com/ted/beziercurve/MainActivity.java
@@ -2,12 +2,32 @@ package com.ted.beziercurve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.ted.bezierview.BezierCurve;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BezierCurve mBC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBC = (BezierCurve) findViewById(R.id.bezier_curve);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mBC.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Animation Show time", Toast.LENGTH_SHORT).show();
+                mBC.beginAnima();
+            }
+        },500);
     }
 }
---
+++ bezierview/src/main/java/com/ted/bezierview/BezierCurve.java
@@ -0,0 +1,119 @@
+package com.ted.bezierview;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Copyright (C) 2008 The Android Open Source Project
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p/>
+ * Created by Ted.Yt on 9/23/16.
+ */
+public class BezierCurve extends View {
+
+    private Paint mPaint;
+    private Path mPath;
+
+    public BezierCurve(Context context) {
+        this(context,null);
+    }
+
+    public BezierCurve(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public BezierCurve(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+        mPaint =  new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(15);
+
+        mPath = new Path();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        /*mPath.moveTo(w / 3, h / 2);//Bezier 曲线的 起点
+        //1 2参数是控制点的坐标，3 4 参数是终点的坐标
+        //mPath.quadTo(w / 2, h / 4, w / 3 * 2, h / 2);
+        mPath.cubicTo(w / 3 + 150, h / 5, w / 3 * 2 - 150, h / 5, w / 3 * 2, h / 2);*/
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int w  = getWidth();
+        int h = getHeight();
+
+        mPath.reset();
+
+        mPath.moveTo(w / 3, h / 2);//Bezier 曲线的 起点
+        //1 2参数是控制点的坐标，3 4 参数是终点的坐标
+        //mPath.quadTo(w / 2, h / 4, w / 3 * 2, h / 2);
+        mPath.cubicTo(  w / 3 + 100 * mValue, h / 3 - 150 * mValue,
+                        w / 3 * 2 - 100 * mValue, h / 3 - 150 * mValue,
+                        w / 3 * 2, h / 2);
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    public void beginAnima(){
+        startAnima();
+    }
+
+    private float mValue;
+    private void startAnima(){
+        ValueAnimator va = ValueAnimator.ofFloat(0f,1f,0f);
+        va.setDuration(3000);
+        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mValue = (float)animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+        va.start();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_UP:
+                startAnima();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+}
